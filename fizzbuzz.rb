8c462cc9809e11bc0b4e7395225f5f82a25d0928
Malcome Fein <mfein90@gmail.com>
2018-07-18 18:23:04 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+# def fizzbuzz
+ 
+# end
+
+def fizzbuzz(int)
+  if int % 3 == 0 and int % 5 == 0 
+    "FizzBuzz"
+  elsif int % 3 == 0 # if the number int is divisible by 3
+    "Fizz" # Go fizz
+  
+  elsif int % 5 == 0 # if the number int is divisible by 5
+    "Buzz" # Go buzz
+  else puts 'null'
+  end
+end
